9aca37f572e9fc8f19732d37e7dbf14dc216fa60
gusaiani <gustavo@poe.ma>
2020-08-27 13:45:33 -0300

    Bind data

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
+import { useAppState } from './AppStateContext'
 import { Column } from './Column'
-import { Card } from './Card'
 import { AppContainer } from './styles'
 import { AddNewItem } from './AddNewItem';
 
 const App = () => {
+  const {state} = useAppState()
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   )
