626aa4c9075304a6e3da53e5acf06d6d6808374a
gusaiani <gustavo@poe.ma>
2020-08-27 12:44:37 -0300

    Autofocus on input through ref

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useFocus } from './utils/useFocus'
 import { NewItemFormContainer, NewItemButton, NewItemInput } from './styles'
 
 interface NewItemFormProps {
@@ -7,11 +8,13 @@ interface NewItemFormProps {
 
 export const NewItemForm = (props: NewItemFormProps) => {
   const [text, setText] = useState(' ')
+  const inputRef = useFocus()
   const { onAdd } = props
 
   return (
     <NewItemFormContainer>
       <NewItemInput
+        ref={inputRef}
         value={text}
         onChange={e => setText(e.target.value)}
       />
